71e0bf2fd6de8bcb491e9d00a47d5b55bf5d8bea
Vincent Kiptoo Cheruiyot <37868729+Vinntoo@users.noreply.github.com>
2022-10-21 13:16:47 +0300

    Mid Gameplay

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,15 +4,212 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
+
+
+    private const float LANE_DISTANCE = 2.0f;
+    private const float TURN_SPEED = 0.05f;
+
+    //
+    private bool isRunning = false;
+
+    // Animation
+    private Animator anim;
+
+    //Movement 
+    private CharacterController controller;
+    private float jumpForce = 6.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    
+    private int desiredLane = 1;  // 0 = left, 1 = middle ,2 = right
+
+    // speed modifier
+    private float originalSpeed = 7.0f;
+    private float speed;
+    private float speedIncreaseLastTick;
+    private float speedIncreaseTime = 2.5f;
+    private float speedIncreaseAmount = 0.1f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        speed = originalSpeed;
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (!isRunning)
+            return;
+
+        if(Time.time - speedIncreaseLastTick > speedIncreaseAmount)
+        {
+            speedIncreaseLastTick = Time.time;
+            speed += speedIncreaseAmount;
+            GameManager.Instance.UpdateModifier(speed - originalSpeed);
+
+            //change the modifier text
+
+        }
+
+
+        // GATHER INPUT ON WHICH LANE WE SHOULD BE
+
+        if (MobileInput.Instance.SwipeLeft)
+            //move left 
+            MoveLane(false);
+
+        if (MobileInput.Instance.SwipeRight)
+            // move right
+            MoveLane(true);
+
+
+        //CALCULATE WHERE WE SHOULD BE IN FUTURE
+
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * LANE_DISTANCE;
+
+        // Lets calculate our move delta
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+
+        bool isGrounded = IsGrounded();
+        anim.SetBool("Grounded", isGrounded);
+
+        //calculate Y
+        if (isGrounded) // if grounded 
+        {
+            verticalVelocity = -0.1f;
+            
+
+            if (MobileInput.Instance.SwipeUp)
+            {
+                //Jump
+                anim.SetTrigger("Jump");
+                verticalVelocity = jumpForce;
+            }
+            else if (MobileInput.Instance.SwipeDown)
+            {
+                //Slide
+                StartSliding();
+                Invoke("StopSliding", 1.0f);
+            }
+
+        }
+        else
+        {
+            verticalVelocity -= (gravity * Time.deltaTime);
+
+               //fast falling mechanic
+            if (MobileInput.Instance.SwipeDown)
+            {
+                    verticalVelocity = -jumpForce;
+            }
+        }
         
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        //Move the Pengu
+        controller.Move(moveVector * Time.deltaTime);
+
+
+        // rotate the pengu to where he is going
+       
+        Vector3 dir = controller.velocity;
+        if (dir != Vector3.zero)
+        {
+            dir.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+        }
+            
+
     }
+
+    private void StartSliding()
+    {
+        anim.SetBool("Sliding", true);
+        controller.height /= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y / 2, controller.center.z);
+
+    }
+
+
+    private void StopSliding()
+    {
+        anim.SetBool("Sliding", false);
+        controller.height *= 2;
+        controller.center = new Vector3(controller.center.x, controller.center.y * 2, controller.center.z);
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+
+        // SAME AS THE ONE BELOW
+
+        //MOVE LEFT 
+
+        //if (!goingRight)
+       // {
+            //desiredLane--;
+            //if(desiredLane == -1)
+               // desiredLane = 0;
+       // }
+
+       // else
+       // {
+           // desiredLane++;
+          //  if(desiredLane == 3)
+               // desiredLane = 2;
+       // }
+
+
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+
+    }
+
+
+    private bool IsGrounded()
+    {
+        Ray groundRay = new Ray(
+            new Vector3(
+                controller.bounds.center.x,
+                (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f,
+                controller.bounds.center.z), 
+            Vector3.down);
+
+        return Physics.Raycast(groundRay, 0.2f + 0.1f);
+    }
+
+    public void StartRunning()
+    {
+        isRunning = true;
+        anim.SetTrigger("StartRunning");
+    }
+
+    private void Crash()
+    {
+        //when player dies
+        anim.SetTrigger("Death");
+        isRunning = false;
+        GameManager.Instance.IsDead = true;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        switch (hit.gameObject.tag)
+        {
+            case "Obstacle":
+                Crash();
+            break;
+        }
+    }
+
 }
